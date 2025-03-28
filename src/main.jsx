b8073583ec6352cbd7b45528a246d882cdbfa2c0
Sebastian Cifuentes Castro <sebastian@MacBook-Pro-de-Sebastian.local>
2025-03-27 22:22:01 -0500

    Cambio en el input de cédula para usar teclado numérico en dispositivos móviles

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/tailwind.css';
-import './styles/responsive.css';  // Asegúrate de tener la ruta correcta
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>

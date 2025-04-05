ab24f053f293c9f9e772099160495d268fed2ca4
Sebastian Cifuentes Castro <sebastian@Mac.lan>
2025-04-05 11:22:17 -0500

    main

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Outlet } from 'react-router-dom';  // Asegúrate de importar esto
 
-const MainLayout = ({ children }) => {
+const MainLayout = () => {
   return (
     <>
       <Header />
-      <main className="pt-[54px] bg-white">{children}</main>
+      <main className="pt-[54px] bg-white">
+        {/* Aquí es donde se renderizan las rutas hijas */}
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
